2f5519f11c5300401ec569dcec511c3e3e1cbb4c
radouaneelabbassi39-ctrl <radouaneelabbassi39@gmail.com>
2026-06-21 15:00:58 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -1,4 +1,4 @@
-import { watchFile, unwatchFile } from 'fs'
+' { watchFile, unwatchFile } from 'fs'
 import fs from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
@@ -15,7 +15,7 @@ global.setting = {
 
 global.info = {
  nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ pairingNumber : '212689698450',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
